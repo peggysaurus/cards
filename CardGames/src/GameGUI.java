815948b228a1f1f0136cf5480feb56f6f4951a56
Peggy <53240898+peggysaurus@users.noreply.github.com>
2019-10-22 21:35:41 +1300

    very basics but unable to start

---
+++ CardGames/src/GameGUI.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GameGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Beth's Game");
+        BorderPane bp = new BorderPane();
+        bp.setCenter(getCenter());
+        Scene scene = new Scene(bp,500,500);
+        primaryStage.setScene(scene);
+    }
+
+    private GridPane getCenter(){
+        GridPane gp = new GridPane();
+
+        return gp;
+    }
+
+}
